c7350880e6960caca93463503ceaf73c4fc31d0c
Aditya Kumar Verma <deekshaverma7860@example.com>
2026-07-24 14:46:11 +0530

    Show MongoDB error

--- init/index.js
+++ init/index.js
@@ -1,11 +1,15 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
+require("dotenv").config();
+
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/cars";
+const MONGO_URL = process.env.MONGO_URI;
+
 main()
   .then(() => {
-    console.log("connected to DB");
+    console.log("Connected to Atlas");
   })
   .catch((err) => {
     console.log(err);
@@ -17,9 +21,12 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
-  initData.data=initData.data.map((obj)=>({...obj,owner:"689d7b6b0f8d4f51e5f9ab12"}));
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "689d7b6b0f8d4f51e5f9ab12",
+  }));
   await Listing.insertMany(initData.data);
-  console.log("data was initialized");
+  console.log("Data was initialized");
 };
 
 initDB();
